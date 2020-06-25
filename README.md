fc5b40804730be949a7cb426c96c85283e2bb455
Sheldon Rupp <sheldon.rupp@epitech.eu>
2020-06-25 11:22:51 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+<p align="center">
+  <h1>phust</h1>
+</p>
+
+<h4> <center>Send RCON commands to your Rust server via the command-line</center></h4>
+
+### Requirements
+* PHP >= 7.2.5
+
+### Installation
+Download the latest binary from the [releases](https://github.com/coef/phust/releases) page.
+
+### Usage
+```sh
+$ chmod +x phust
+$ ./phust rcon:send --help
+$ ./phust rcon:send --ip "server.ip" --port "28016" --pass "password" --command "status"
+```
+
+------
+
+## License
+
+Phust is an open-source software licensed under the [MIT license](https://github.com/coef/phust/blob/master/LICENSE.md).
---
+++ app/Commands/SendRconCommand.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Commands;
+
+use Illuminate\Console\Scheduling\Schedule;
+use LaravelZero\Framework\Commands\Command;
+use WebSocket\Client;
+
+class SendRconCommand extends Command
+{
+    /**
+     * The signature of the command.
+     *
+     * @var string
+     */
+    protected $signature = 'rcon:send {--command= : Command to be sent to server (required)}
+                                      {--ip= : IP of server (required)}
+                                      {--port= : Port of server (required)}
+                                      {--pass= : RCON Password (required)}';
+
+    /**
+     * The description of the command.
+     *
+     * @var string
+     */
+    protected $description = 'Send RCON command';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $needs = [
+            'command',
+            'ip',
+            'port',
+            'pass',
+        ];
+        foreach ($needs as $need) {
+            if (!array_key_exists($need, $this->options()) || empty($this->options()[$need])) {
+                $this->error('Please pass all required options');
+                exit(1);
+            }
+        }
+        $client = new Client("ws://{$this->option('ip')}:{$this->option('port')}/{$this->option('pass')}");
+        $data = json_encode([
+            'Identifier' => 0,
+            'Message' => $this->option('command'),
+            'Stacktrace' => '',
+            'Type' => 3
+        ]);
+        $client->send($data);
+        $result = json_decode($client->receive());
+        echo $result->Message;
+    }
+}
